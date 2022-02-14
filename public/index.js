a012ae1e7a273d602e1fcaf68afb68de4b9cfedf
Roardevil <pateldeep25022001@gmail.com>
2022-02-14 16:15:40 +0000

    fvuyu

--- public/index.js
+++ public/index.js
@@ -88,7 +88,7 @@ let app = new Vue({
                 this.cart.push(id);
             }
 
-            fetch('https://deepwebapp.herokuapp.com/collection/products' + this.product[space]._id, {
+            fetch('https://deepwebapp.herokuapp.com/collection/products' + this.product[space].id, {
 
                 method: 'PUT', //Set the HTTP method as 'PUT'
 
@@ -173,49 +173,18 @@ let app = new Vue({
 
         },
 
-        getproduct() {
-            // Sort by alphabetical order
-            searchproduct = searchproduct.sort((a, b) => {
-                if (this.sortBy == 'location') {
-                    let fa = a.location.toLowerCase(), fb = b.location.toLowerCase()
-
-                    if (fa < fb) {
-                        return -1
-                    }
-                    if (fa > fb) {
-                        return 1
-                    }
-                    return 0
 
 
-                } else if (this.sortBy == 'price') {
-                    return a.price - b.price
-                }
-
-
-                else if (this.sortBy == 'space') {
-                    return a.space - b.space
-                }
-            })
-
-
-            // Show sorted array in descending or ascending order
-            if (!this.ascending) {
-                searchproduct.reverse()
-            }
-
-            return searchproduct
+        created: function () {
+            // replace the URL to your Heroku app and route
+            fetch('https://deepwebapp.herokuapp.com/collection/products').then(
+                function (response) {
+                    response.json().then(
+                        function (json) {
+                            // note that we used 'store.product' instead of 'this.product'
+                            app.product = json;
+                        });
+                })
         }
-    },
-    created: function () {
-        // replace the URL to your Heroku app and route
-        fetch('https://deepwebapp.herokuapp.com/collection/products').then(
-            function (response) {
-                response.json().then(
-                    function (json) {
-                        // note that we used 'store.product' instead of 'this.product'
-                        app.product = json;
-                    });
-            })
     }
 })
